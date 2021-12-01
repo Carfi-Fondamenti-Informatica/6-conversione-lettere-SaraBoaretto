87241ca09a55f5900edd649750a1718c33ad8d4c
SaraBoaretto <91607833+SaraBoaretto@users.noreply.github.com>
2021-12-01 19:44:54 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,20 @@
-
+char uno (char b){
+  bool r;
+  char d;
+  if ((b>=65 && b<=90)||(b>=97 && b<=122)){
+    r = true;
+    if ((b>=65) && (b<=90)){
+      d= b+32;
+      }else if ((b>=97) && (b<=122)){
+      d=b-32;
+      }
+    }else{
+    r=false;
+    d=1;
+    }
+  return d;
+  
+  }
+    
+    
+   
